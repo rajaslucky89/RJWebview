a72cc2025c32c4e66002db3ee097237ee941d986
Raja Syahmudin <rajaslucky89@gmail.com>
2020-08-12 16:14:21 +0700

    Initial Commit

---
+++ RJWebView/RJWebView.h
@@ -0,0 +1,19 @@
+//
+//  RJWebView.h
+//  RJWebView
+//
+//  Created by Raja Syahmudin on 12/08/20.
+//  Copyright © 2020 RJWebView. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RJWebView.
+FOUNDATION_EXPORT double RJWebViewVersionNumber;
+
+//! Project version string for RJWebView.
+FOUNDATION_EXPORT const unsigned char RJWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RJWebView/PublicHeader.h>
+
+
